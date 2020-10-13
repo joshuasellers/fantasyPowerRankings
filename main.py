397b1877bfbe3020e46d9c067b138fc3a32da06a
Josh Sellers <joshuasellersd@gmail.com>
2020-10-12 22:58:40 -0700

    split up into metrics class

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-import requests
-import consts
+import metrics
 
-scores = {}
-league_id = 251025
-year = 2020
-url = "https://fantasy.espn.com/apis/v3/games/ffl/seasons/2020/segments/0/leagues/251025"
 
-r = requests.get(url, cookies={'SWID': consts.SWID(), 'espn_s2': consts.espn_s2()})
+def main():
+    df = metrics.get_avg()
+    print(df.head())
 
-d = r.json()
 
-print(d)
-
+main()
